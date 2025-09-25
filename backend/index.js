82fd449db9e0e172f63f8645c729efee89d44525
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2025-09-25 10:13:49 +0200

    Implement user authentication with JWT.

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,19 @@ import mongoose from "mongoose";
 import Weather from "./models/Weather.js";
 import Crop from "./models/Crop.js";
 
+import authRoutes from "./routes/auth.js";
+import { auth } from "./middleware/auth.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/api/user/me", auth, (req, res) => {
+  res.json({ message: "Welcome!", user: req.user });
+});
 
 app.get("/", (req, res) => {
   res.send("🌱 Agriculture API Running");
